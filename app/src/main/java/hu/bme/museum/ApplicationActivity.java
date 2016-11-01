1399775dd3df79abc61a60147bf01c8465ed9ae1
centend <centend@users.noreply.github.com>
2016-11-01 21:50:25 +0100

    Added basic artwork browsing

--- app/src/main/java/hu/bme/museum/ApplicationActivity.java
+++ app/src/main/java/hu/bme/museum/ApplicationActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.widget.TextView;
 
 public class ApplicationActivity extends FragmentActivity {
 
--- app/src/main/java/hu/bme/museum/ExhibitionsFragment.java
+++ app/src/main/java/hu/bme/museum/ExhibitionsFragment.java
@@ -6,15 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ExhibitionsFragment extends TabFragment {
+
+    LinearLayout artworksLinearLayout;
+    LayoutInflater inflater;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        this.inflater = inflater;
         View rootView = inflater.inflate(R.layout.fragment_exhibitions, null, false);
+
+        artworksLinearLayout = (LinearLayout) rootView.findViewById(R.id.artworksLinearLayout);
+
+        addArtworks();
+
         return rootView;
+    }
+
+    public void addArtworks() {
+        for (int i = 0; i < 3; ++i) {
+            View newArtwork = inflater.inflate(R.layout.artwork, null);
+            TextView title = (TextView) newArtwork.findViewById(R.id.artworkTitleTextView);
+            title.setText(title.getText() + " " + (i+1));
+            artworksLinearLayout.addView(newArtwork, 0);
+        }
     }
 
     @Override
