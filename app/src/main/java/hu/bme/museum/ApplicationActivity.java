31ee481c8c0876b42710a860eeaf819aefaabedb
centend <centend@users.noreply.github.com>
2016-11-01 19:19:27 +0100

    Fixed merge issue

--- app/src/main/java/hu/bme/museum/ApplicationActivity.java
+++ app/src/main/java/hu/bme/museum/ApplicationActivity.java
@@ -2,7 +2,6 @@ package hu.bme.museum;
 
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
@@ -13,40 +12,14 @@ public class ApplicationActivity extends FragmentActivity {
 
     public static final String LOGIN_SUCCESSFUL = "Login successful!";
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
 
-<<<<<<< HEAD
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         viewPager.setAdapter(new ApplicationFragmentPagerAdapter(getSupportFragmentManager(),
                 ApplicationActivity.this));
-=======
-        Toast.makeText(ApplicationActivity.this, LOGIN_SUCCESSFUL, Toast.LENGTH_SHORT).show();
-
-        initUI();
     }
-
-    private void initUI() {
-        btGame = (Button) findViewById(R.id.btGame);
-        btExhibitions = (Button) findViewById(R.id.btExhibitions);
-        btMap = (Button) findViewById(R.id.btMap);
-
-        btGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    startGame();
-            }
-        });
->>>>>>> 0e75d6e010db81c8fc3b0a832e12b781c17e6c5b
-
-        // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.slidingTabs);
-        tabLayout.setupWithViewPager(viewPager);
-
-    }
-
 }
