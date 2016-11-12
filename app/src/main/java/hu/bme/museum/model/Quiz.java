3dee55b9fbd95730dcfdb73b2f6d267a16013e29
Ujhelyi Benedek <ujhelyi.benedek@gmail.com>
2016-11-12 20:03:47 +0100

    Quiz class

---
+++ app/src/main/java/hu/bme/museum/model/Quiz.java
@@ -0,0 +1,49 @@
+package hu.bme.museum.model;
+
+public class Quiz {
+    public String question;
+    public String A;
+    public String B;
+    public String C;
+    public String D;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getA() {
+        return A;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setA(String a) {
+        this.A = a;
+    }
+
+    public void setB(String b) {
+        this.B = b;
+    }
+
+    public void setC(String c) {
+        this.C = c;
+    }
+
+    public void setD(String d) {
+        this.D = d;
+    }
+}
