78154bc0b8634f6c11041c470f4447cf6604b713
centend <centend@users.noreply.github.com>
2016-11-27 12:10:34 +0100

    Back button works for moving between tabs. Does not work for back stack of child fragments

--- app/src/main/java/hu/bme/museum/activities/ApplicationActivity.java
+++ app/src/main/java/hu/bme/museum/activities/ApplicationActivity.java
@@ -16,28 +16,28 @@ import android.widget.Toast;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+import java.util.Stack;
 
 import hu.bme.museum.db.FirebaseAdapter;
 import hu.bme.museum.fragments.ApplicationFragmentPagerAdapter;
 import hu.bme.museum.R;
-import hu.bme.museum.model.User;
+import hu.bme.museum.fragments.TabFragment;
 
 public class ApplicationActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "ApplicationActivity";
 
-    GoogleApiClient googleApiClient;
-    ApplicationFragmentPagerAdapter fragmentPagerAdapter;
+    private GoogleApiClient googleApiClient;
+
+    private ViewPager viewPager;
+    private ApplicationFragmentPagerAdapter fragmentPagerAdapter;
+    private TabLayout tabLayout;
+
+    private Stack<Integer> tabBackStack = new Stack<>();
+    private int previousTab = 0;
+    private boolean enableSaveToHistory = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,14 +45,30 @@ public class ApplicationActivity extends AppCompatActivity implements GoogleApiC
         setContentView(R.layout.activity_application);
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
         fragmentPagerAdapter = new ApplicationFragmentPagerAdapter(getSupportFragmentManager(),
                 ApplicationActivity.this);
         viewPager.setAdapter(fragmentPagerAdapter);
 
         // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.slidingTabs);
+        tabLayout = (TabLayout) findViewById(R.id.slidingTabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (enableSaveToHistory) {
+                    tabBackStack.push(previousTab);
+                    previousTab = tab.getPosition();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
 
         //Set topmost toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.topmost_toolbar);
@@ -66,7 +82,23 @@ public class ApplicationActivity extends AppCompatActivity implements GoogleApiC
                 .build();
     }
 
+    @Override
+    public void onBackPressed() {
+        TabFragment currentTabFragment =
+                (TabFragment) fragmentPagerAdapter.getItem(tabLayout.getSelectedTabPosition());
 
+        if (currentTabFragment.isBackStackEmpty()) {
+            if (tabBackStack.isEmpty()) {
+                super.onBackPressed();
+            } else {
+                enableSaveToHistory = false;
+                viewPager.setCurrentItem(tabBackStack.pop());
+                enableSaveToHistory = true;
+            }
+        } else {
+            currentTabFragment.onBackPressed();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/hu/bme/museum/fragments/TabFragment.java
+++ app/src/main/java/hu/bme/museum/fragments/TabFragment.java
@@ -2,7 +2,22 @@ package hu.bme.museum.fragments;
 
 import android.support.v4.app.Fragment;
 
+import java.util.Stack;
+
 public abstract class TabFragment extends Fragment {
+    private Stack<Integer> backStack;
+
     abstract public String getTabTitle();
 
+    public void changeChildFragment(Fragment newFragment) {
+
+    }
+
+    public boolean isBackStackEmpty() {
+        return true;
+    }
+
+    public void onBackPressed() {
+        getFragmentManager().popBackStack();
+    }
 }
